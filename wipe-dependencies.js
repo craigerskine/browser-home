5eaddf488b657ec99bd9db5aadaabb71ccd11628
Craig Erskine <craig@craigerskine.com>
2019-02-20 11:37:51 -0600

    Update wipe-dependencies.js

--- wipe-dependencies.js
+++ wipe-dependencies.js
@@ -1,17 +1,17 @@
-const fs = require('fs')
-const wipeDependencies = () => {
-  const file  = fs.readFileSync('package.json')
-  const content = JSON.parse(file)
-  for (const devDep in content.devDependencies) {
-    content.devDependencies[devDep] = '*'
-  }
-  for (const dep in content.dependencies) {
-    content.dependencies[dep] = '*'
-  }
-  fs.writeFileSync('package.json', JSON.stringify(content))
-}
+var fs = require('fs'),
+  wipeDependencies = function() {
+    var file  = fs.readFileSync('package.json'),
+      content = JSON.parse(file);
+    for (var devDep in content.devDependencies) {
+      content.devDependencies[devDep] = '*';
+    }
+    for (var dep in content.dependencies) {
+      content.dependencies[dep] = '*';
+    }
+    fs.writeFileSync('package.json', JSON.stringify(content,null,2));
+  };
 if (require.main === module) {
-  wipeDependencies()
+  wipeDependencies();
 } else {
-  module.exports = wipeDependencies
+  module.exports = wipeDependencies;
 }
