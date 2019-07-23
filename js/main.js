325eb58ef4f199cb2362f11fd2ded37f9ad616ca
Craig Erskine <craig@craigerskine.com>
2019-07-22 23:18:15 -0500

    re-written in vanilla js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-function dateTime() {
-  $('.date').html(moment().format('MMMM Do YYYY'));
-  $('.hour').html(moment().format('hh'));
-  $('.minute').html(moment().format('mm'));
-  $('.second').html(moment().format('ss'));
-  $('.beat').html(((moment().utcOffset(1).seconds() + (moment().utcOffset(1).minutes() * 60) + (moment().utcOffset(1).hours() * 3600)) / 86.4).toFixed(2));
+function time(){
+  var date = new Date();
+  hours = date.getHours() % 12;
+  hours = hours ? hours : 12;
+  hours = pad(hours);
+  minutes = pad(date.getMinutes());
+  seconds = pad(date.getSeconds());
+  beatHours = (date.getUTCHours() == 23) ? 0 : date.getUTCHours() + 1;
+  var beats = Math.abs(((((beatHours * 60) + date.getUTCMinutes()) * 60) + date.getUTCSeconds()) / 86.4).toFixed(2);
+  var today = date.toLocaleDateString("en-US", {year: 'numeric', month: 'long', day: 'numeric'});
+
+  document.querySelectorAll('.hours').forEach(function(item){item.innerHTML = hours});
+  document.querySelectorAll('.minutes').forEach(function(item){item.innerHTML = minutes});
+  document.querySelectorAll('.seconds').forEach(function(item){item.innerHTML = seconds});
+  document.querySelectorAll('.beats').forEach(function(item){item.innerHTML = beats});
+  document.querySelectorAll('.date').forEach(function(item){item.innerHTML = today})
 }
 
-dateTime();
-setInterval(dateTime, 1000);
+function pad(num){
+  return ('0' + num).slice(-2);
+}
+
+time();
+setInterval(time, 1000);
